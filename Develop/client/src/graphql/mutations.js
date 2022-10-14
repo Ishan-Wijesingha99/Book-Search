9d58e7ddc38125c41ec84a2aa1cafc45a5c8bd93
Ishan <ishanwijes@gmail.com>
2022-10-15 05:49:11 +1100

    created apollo client and created mutations and queries

---
+++ Develop/client/src/graphql/mutations.js
@@ -0,0 +1,90 @@
+// import gql statement
+import { gql } from '@apollo/client'
+
+
+
+// LOGIN_USER graphql mutation
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`
+
+// ADD_USER graphql mutation
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`
+
+// SAVE_BOOK graphql mutation
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: bookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`
+
+// REMOVE_BOOK graphql mutation
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`
---
+++ Develop/client/src/graphql/queries.js
@@ -0,0 +1,24 @@
+// import gql statement
+import { gql } from '@apollo/client'
+
+
+
+// GET_ME graphql query
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`
