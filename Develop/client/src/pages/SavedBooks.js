da84e15f8364c01ed8707872813bf1c5de1e4d3b
Ishan <ishanwijes@gmail.com>
2022-10-16 01:00:09 +1100

    search and save books functionality completed

--- Develop/client/src/pages/SavedBooks.js
+++ Develop/client/src/pages/SavedBooks.js
@@ -1,58 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
+// import useMutation and useQuery hook
+import { useMutation, useQuery } from '@apollo/client'
+// import REMOVE_BOOK mutation
+import { REMOVE_BOOK } from '../graphql/mutations'
+// import GET_ME query
+import { GET_ME } from '../graphql/queries'
+
+
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  // destructure data and loading boolean from GET_ME graphql query
+  const { data, loading } = useQuery(GET_ME)
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  // define userData as a variable that can be changed
+  let userData
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  if(data) {
+    // if data exists, change userData value to data.me
+    userData = data.me
+  } else {
+    // if data does not exist, make userData an empty object
+    userData = {}
+  }
 
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
+  // destructure removeBook function from REMOVE_BOOK graphql mutation
+  const [removeBook] = useMutation(REMOVE_BOOK)
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+    if (!token) return false
 
     try {
-      const response = await deleteBook(bookId, token);
+      const { data } = await removeBook({ variables: { bookId } })
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      if (data == null) throw new Error('something went wrong!')
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -60,10 +48,8 @@ const SavedBooks = () => {
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
+  // this is the loading screen, when loading is true, this h2 element will be returned
+  if (loading) return <h2>LOADING...</h2>
 
   return (
     <>
